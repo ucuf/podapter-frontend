828c5bc00e56f76b4abce4b04f0221d5418b99d9
mehdi ouledhali <mehdi.ouledhali@gmail.com>
2023-04-27 22:08:59 +0000

    feat: add stripe pricing table

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,12 +1,14 @@
 import PageLayout from "./layout/PageLayout";
 
 
-export default function Pricing(){
-    return (
-        <PageLayout title={"Pricing"}>
-            <div>
-                Pricing Page
-            </div>
-        </PageLayout>
-    )
-}
+
+export default function Pricing() {
+
+  return (
+    <PageLayout title={"Pricing"}>
+        <stripe-pricing-table pricing-table-id="prctbl_1N1bmVKpaH2wiwbEVmGAmV1Z"
+        publishable-key="pk_test_51N0LFTKpaH2wiwbEKAv4v8a9ii4X8oPcjjzGx3ktJfI0awLUTNFrBhNweCT4l6YTu7wi0ZcXGbNk3dMW3IpXyClf00vb4vipcr">
+        </stripe-pricing-table>
+    </PageLayout>
+  );
+};
